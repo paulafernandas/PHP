83e566f11ccca9e25921f88c632d51e37f8a83f8
Paula Fernanda da Silva <71718229+paulafernandas@users.noreply.github.com>
2024-10-10 21:48:16 -0300

    Add files via upload
    Exemplos de condicionais em PHP.

---
+++ Condicionais/If.php
@@ -0,0 +1,16 @@
+<?php
+
+$nota1 = 10;
+$nota2 = 5;
+
+$media = ($nota1 + $nota2) / 2;
+
+if ($media > 7) {
+
+    echo "Aprovado";
+
+} else {
+
+    echo "Reprovado";
+
+}
---
+++ Condicionais/Switch.php
@@ -0,0 +1,15 @@
+<?php
+
+switch ($i=2) {
+    case 0:
+        echo "i é igual a 0";
+        break;
+
+    case 1:
+        echo "i é igual a 1.";
+        break;
+
+    case 2:
+        echo "i é igual 2.";
+        break;
+}
