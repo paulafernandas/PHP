74f8e769d1a22918f481ace90b72a9818dc5b2da
Paula Fernanda da Silva <71718229+paulafernandas@users.noreply.github.com>
2024-10-10 22:00:13 -0300

    Add files via upload

---
+++ Condicionais/Tabuada5.php
@@ -0,0 +1,17 @@
+<?php
+// Tabuada do 5
+// $numero = 5;
+
+// for ($i = 1; $i <= 10; $i++) {
+//     echo "$numero x $i = " . ($numero * $i) . "\n";
+// }
+
+$numero = 15;
+$i = 1;
+
+while ($i <= 10) {
+    echo "$numero x $i = " . ($numero * $i) . "\n";
+    $i++;
+}
+
+?>
