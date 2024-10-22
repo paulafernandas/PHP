dc8ead9fe2c758b3239c96444caf70b0b365ecb5
Paula Fernanda da Silva <71718229+paulafernandas@users.noreply.github.com>
2024-10-21 21:55:54 -0300

    Update Produto.php

--- Produto.php
+++ Produto.php
@@ -32,14 +32,25 @@ class Alimento extends Produto{
 
 private String $validade;
 
-public function __construct(String $nome, String $preco, int $validade){
+public function __construct(String $nome, String $preco, String $validade){
     parent::__construct($nome, $preco);
-    $this->validade = $validade;''
+    $this->validade = $validade;
 }
+}
+
+class Roupa extends Produto{ 
+
+    private int $tamanho;
+    
+    public function __construct(String $nome, String $preco, int $tamanho){
+        parent::__construct($nome, $preco);
+        $this->tamanho = $tamanho;
+    }
 }
 
 //Criando os objetos das classes ELETRONICO e ALIMENTO
 $produtoEletronico = new Eletronico ("Smartphone", "R$ 1200,00", 12);
 $produtoAlimento = new Alimento ("Maçã", "R$ 0,50", "30/12/2024");
+$produtoRoupa = new Roupa("Calça", "R$ 100,00", 40);
 
 ?>
